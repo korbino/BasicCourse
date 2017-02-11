9b4832026f7930a5c158915d2c30641ee5d285c0
Yurij Shinkaruk <shinkaruk.yura@gmail.com>
2017-02-11 15:39:31 +0200

    small update

--- Lesson2/Lesson2/Program.cs
+++ Lesson2/Lesson2/Program.cs
@@ -10,38 +10,29 @@ namespace Lesson2
     {
         static void Main(string[] args)
         {
-            //for (int i = 1; i <= 100; i++)
-            //{
-            //    if ((i % 3 == 0) && (i % 5 == 0))
-            //    {                    
-            //        Console.WriteLine("FizzBuzz");
-            //        continue; 
-            //    }else if (i % 3 == 0) {
-            //        Console.WriteLine("Fizz");
-            //        continue; 
-            //    }else if (i % 5 == 0) {
-            //        Console.WriteLine("Buzz");
-            //        continue;
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine(i);
-            //    }                
-            //}
-            //Console.ReadLine();
-            Function_1();
-        }
-
-        static void Function_1()
-        {
-            int val = 1;
-            Function_2(ref val);
-            Console.WriteLine("VAL:{0}", val);
+            for (int i = 1; i <= 100; i++)
+            {
+                if ((i % 3 == 0) && (i % 5 == 0))
+                {
+                    Console.WriteLine("FizzBuzz");
+                    continue;
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                    continue;
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
             Console.ReadLine();
-        }
-        static void Function_2(ref int value)
-        {
-            value = 20;
         }
     }
 }
