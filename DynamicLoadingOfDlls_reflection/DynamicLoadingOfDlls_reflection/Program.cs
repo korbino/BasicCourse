51dadcf3bf5b32d6a6f35a55f56fa62747a82b19
Yurij Shinkaruk <shinkaruk.yura@gmail.com>
2017-04-28 17:06:57 +0300

    update dymaic loading example

--- DynamicLoadingOfDlls_reflection/DynamicLoadingOfDlls_reflection/Program.cs
+++ DynamicLoadingOfDlls_reflection/DynamicLoadingOfDlls_reflection/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
+using Test;
 
 namespace DynamicLoadingOfDlls_reflection
 {
@@ -11,15 +12,19 @@ namespace DynamicLoadingOfDlls_reflection
     {
         static void Main(string[] args)
         {
-            var DLL = Assembly.LoadFile(@"C:\dll\ExampleDll.dll");
-
-            foreach (Type type in DLL.GetExportedTypes())
+            var DLL = Assembly.LoadFile(@"C:\dll\ExampleDll.dll");            
+            foreach (Type type in DLL.GetTypes())
             {
-                var c = Activator.CreateInstance(type);
-                //c.DoSomething();
+                bool isImplementedInterface = typeof(ITest).IsAssignableFrom(type);
+                if (isImplementedInterface)
+                {
+                    Console.WriteLine("Dll is implementing ITest interface");
+                }
+
+                var c = Activator.CreateInstance(type);            
                 type.InvokeMember("DoSomething", BindingFlags.InvokeMethod, null, c, new object[]{});
-                
-                
+                //dynamic c = Activator.CreateInstance(type);
+                //c.DoSomething();                        
             }
 
             Console.ReadLine();
--- DynamicLoadingOfDlls_reflection/ExampleDll/ExampleDll/TestDll.cs
+++ DynamicLoadingOfDlls_reflection/ExampleDll/ExampleDll/TestDll.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Test;
 
 namespace ExampleDll
 {
-    public class TestDll
+    public class TestDll : ITest
     {
         public void DoSomething(){
-            Console.WriteLine("Doing something....");
+            Console.WriteLine("Doing something.... from: " + this);
         }
     }
 }
